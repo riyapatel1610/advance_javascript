6bc878e1b46be28c6c7dcbdbc9a56af8b7b786a6
Riya patel <87262995+riyapatel1610@users.noreply.github.com>
2023-10-06 22:45:37 +0530

    Add files via upload

---
+++ task7_familySon_By_ID/family_son.js
@@ -0,0 +1,108 @@
+let grandfather = {
+    name: "Balubhai",
+    age: 85,
+    child: [
+      {
+        id: 1001,
+        name: "vinubhai",
+        age: 65,
+        occupation: "retired",
+        vehicle: [
+          {
+            name: "activa",
+            number: 1010,
+          },
+          {
+            name: "nissan micra",
+            number: 6549,
+          },
+          {
+            name: "xuv700",
+            number: 6569,
+          },
+        ],
+        son: [
+          {
+            name: "vivek",
+            age: 38,
+          },
+          {
+            name: "sameer",
+            age: 35,
+          },
+          {
+            name: "sanket",
+            age: 30,
+          },
+        ],
+      },
+      {
+        id: 1002,
+        name: "dhirajbhai",
+        age: 50,
+        occupation: "farmar",
+        vehicle: [
+          {
+            name: "ather",
+            number: 1737,
+          },
+          {
+            name: "soda",
+            number: 8743,
+          },
+          {
+            name: "swift",
+            number: 1610,
+          },
+        ],
+        son: [
+          {
+            name: "riya",
+            age: 21,
+          },
+          {
+            name: "meet",
+            age: 18,
+          },
+        ],
+      },
+      {
+        id: 1003,
+        name: "pradipbhai",
+        age: 45,
+        occupation: "farmar",
+        vehicle: [
+          {
+            name: "vespa",
+            number: 4185,
+          },
+          {
+            name: "swift",
+            number: 3559,
+          },
+        ],
+        son: [
+          {
+            name: "drashil",
+            age: 20,
+          },
+          {
+            name: "kenil",
+            age: 17,
+          },
+        ],
+      },
+    ],
+  };
+
+
+function FindSonName(id){
+    let x=grandfather.child.find((element)=>{
+        return element.id==id;
+    });
+    let PrintName=x.son.map((element)=>{
+        return element["name"];
+    });
+    return PrintName;
+}
+console.log(FindSonName(1002));
